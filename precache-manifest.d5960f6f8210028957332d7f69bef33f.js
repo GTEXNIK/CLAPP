d6034fec11b6c605c9b95683ee15c5fb21588db5
GTEXNIK <65672058+GTEXNIK@users.noreply.github.com>
2020-07-28 14:23:11 +0300

    Updates

---
+++ precache-manifest.d5960f6f8210028957332d7f69bef33f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7056056d8f9ecf030e464bc1d31705",
+    "url": "/CLAPP/index.html"
+  },
+  {
+    "revision": "9179cecd4e89fe6d3250",
+    "url": "/CLAPP/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "16ea7f025a78a784ed24",
+    "url": "/CLAPP/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9179cecd4e89fe6d3250",
+    "url": "/CLAPP/static/js/2.9eace588.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/CLAPP/static/js/2.9eace588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ea7f025a78a784ed24",
+    "url": "/CLAPP/static/js/main.09549674.chunk.js"
+  },
+  {
+    "revision": "37102bdd09ac8efb2a07",
+    "url": "/CLAPP/static/js/runtime-main.1411dbea.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/CLAPP/static/media/persik.4e1ec840.png"
+  }
+]);
