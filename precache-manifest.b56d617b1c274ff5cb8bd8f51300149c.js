5b704a9c3b8be4938bed764ea34d52edd4f3376f
GTEXNIK <65672058+GTEXNIK@users.noreply.github.com>
2020-08-18 21:42:59 +0300

    Updates

---
+++ precache-manifest.b56d617b1c274ff5cb8bd8f51300149c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17093ac21cf5b5599e6cbf8c9a2bd45",
+    "url": "/CLAPP/index.html"
+  },
+  {
+    "revision": "014a21852f0d3ce94585",
+    "url": "/CLAPP/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "4b8fdb08dbc00c9ca4d7",
+    "url": "/CLAPP/static/css/main.d9817ee9.chunk.css"
+  },
+  {
+    "revision": "014a21852f0d3ce94585",
+    "url": "/CLAPP/static/js/2.1bb0486e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/CLAPP/static/js/2.1bb0486e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8fdb08dbc00c9ca4d7",
+    "url": "/CLAPP/static/js/main.6de463d0.chunk.js"
+  },
+  {
+    "revision": "ab6b2f38fa54a629f30f",
+    "url": "/CLAPP/static/js/runtime-main.49ac8d0b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CLAPP/precache-manifest.fa34fe9be25582534b71f3810b23b1fb.js"
+  "/CLAPP/precache-manifest.b56d617b1c274ff5cb8bd8f51300149c.js"
 );
 
 self.addEventListener('message', (event) => {
