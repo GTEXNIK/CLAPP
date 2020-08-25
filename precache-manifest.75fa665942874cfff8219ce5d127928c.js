2de6cb0f78a5a7a1c88ea2d116e646c28b6f89e7
GTEXNIK <65672058+GTEXNIK@users.noreply.github.com>
2020-08-25 20:37:38 +0300

    Updates

--- precache-manifest.75fa665942874cfff8219ce5d127928c.js
+++ precache-manifest.75fa665942874cfff8219ce5d127928c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "010ab4c3672b5102015ef028a8e6aefe",
+    "revision": "7f7a4f2a84818e828b4be28d73631aed",
     "url": "/CLAPP/index.html"
   },
   {
-    "revision": "014a21852f0d3ce94585",
+    "revision": "f514a423064d00bb7c9d",
     "url": "/CLAPP/static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "78beae3ce5a0273cb34f",
+    "revision": "4dc287df0c2ce8a14b22",
     "url": "/CLAPP/static/css/main.d9817ee9.chunk.css"
   },
   {
-    "revision": "014a21852f0d3ce94585",
-    "url": "/CLAPP/static/js/2.1bb0486e.chunk.js"
+    "revision": "f514a423064d00bb7c9d",
+    "url": "/CLAPP/static/js/2.904f0502.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "/CLAPP/static/js/2.1bb0486e.chunk.js.LICENSE.txt"
+    "url": "/CLAPP/static/js/2.904f0502.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "78beae3ce5a0273cb34f",
-    "url": "/CLAPP/static/js/main.2f40c28f.chunk.js"
+    "revision": "4dc287df0c2ce8a14b22",
+    "url": "/CLAPP/static/js/main.75a90193.chunk.js"
   },
   {
     "revision": "ab6b2f38fa54a629f30f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CLAPP/precache-manifest.d0b2606e840ff9b3d8db4d923f716a93.js"
+  "/CLAPP/precache-manifest.75fa665942874cfff8219ce5d127928c.js"
 );
 
 self.addEventListener('message', (event) => {
